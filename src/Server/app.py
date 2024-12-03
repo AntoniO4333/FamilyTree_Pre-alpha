8e6826fe4f79ea3054a20f4e0a3d0d6e57ae16ae
Anton Cheremushkin <anton43r3m@gmail.com>
2024-12-03 14:00:50 +0700

    Теперь фронтенд включается, но с беком не коннектится

--- src/Server/app.py
+++ src/Server/app.py
@@ -5,12 +5,22 @@ app = Flask(__name__)
 # Маршрут для страницы логина
 @app.route("/")
 def login():
-    return send_from_directory("src/Client/Login", "login.html")
+    return send_from_directory("../../src/Client/Login", "login.html")
 
-# Маршрут для статических файлов (CSS, JS, изображения)
-@app.route('/<path:filename>')
-def static_files(filename):
-    return send_from_directory("src/Client/Login", filename)
+# Маршрут для CSS-файлов
+@app.route('/styles.css')
+def serve_css():
+    return send_from_directory("../../src/Client/Login", "styles.css")
+
+# Маршрут для JS-файлов
+@app.route('/script.js')
+def serve_js():
+    return send_from_directory("../../src/Client/Login", "script.js")
+
+# Маршрут для изображений
+@app.route('/Images/<filename>')
+def serve_images(filename):
+    return send_from_directory("../../src/Client/Images", filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
